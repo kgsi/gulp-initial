0bbd544cfd10680f17c0b3d4c8dcfef825e50b37
e-bird@kogiso <kogiso@e-bird.biz>
2015-10-03 20:57:36 +0900

    [fix]タスク修正

--- gulp/tasks/dest.js
+++ gulp/tasks/dest.js
@@ -5,18 +5,10 @@ var gulp          = require('gulp'),
 	config        = require('../config'),
 	gulpif        = require('gulp-if');
 
-
-// dest
 gulp.task('dest',function(callback){
 	// CLI解析（--upload） 
 	var cliOption = minimist(process.argv.slice(3));
 	var isUpload = (cliOption.upload) ? true : false;
 
-	if(!isUpload){
-		// --upload 無し
-		runSequence('clean', ['compass','ejs'], 'copy', ['image', 'libs']);
-	}else{
-		// --upload 有り
-		runSequence('clean', ['compass','ejs'], 'copy', ['image', 'libs'], 'upload');
-	}
+	runSequence('clean', ['compass','ejs'], 'copy', ['image', 'libs']);
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
 var requireDir = require('require-dir');
 
+console.log('test');
 requireDir('./gulp/tasks', { recurse: true });
